0ceb1b7959d0faa0b10d8f03fdd37edc32f44218
ck-js <ckentertainmentinfo@gmail.com>
2024-10-04 20:31:40 +0700

    we loop over each item in the edge list using a for each loop to be able to loop inside each edge list item which consists of vertices which we dig into using another for each loop. We then check if our unique array does not include the vertex value which will only add unique vertex values from all edge list items, we then sort the unique array from ascending order. We now know the dimensions to initialize our adjacency matrix with zeros to start with which uses the unique array length to create a new array with the length derived from the unique array length and then call the fill method and pass in 0 to fill all row items with 0 then repeat for next row until loop is false.

--- script.js
+++ script.js
@@ -7,5 +7,23 @@ const edgeList = [
   [5,6],
   [2,4],  
 ];
+// directed graph represented as an adjacency matrix
+// get unique vetices from edge list
+let vertices = []
+edgeList.forEach(edge => {
+    edge.forEach(vertex => {
+        if (!vertices.includes(vertex)) {
+        vertices.push(vertex);
+        }
+    });
+    })
+vertices.sort((a, b) => a - b);
+
+// create adjacency matrix
+let adjacencyMatrix = []
+for (let i = 0; i < vertices.length; i++) {
+    adjacencyMatrix.push(new Array(vertices.length).fill(0));
+}
+console.log(adjacencyMatrix);
 
 
